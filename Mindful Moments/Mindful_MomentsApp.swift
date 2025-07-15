0f75e2bf39f44a75c4d12b995a4f1d5f7eeda906
Aanya Mahapatra <aanya.mahapatra@gmail.com>
2025-07-15 10:03:33 -0500

    Initial Commit

---
+++ Mindful Moments/Mindful_MomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mindful_MomentsApp.swift
+//  Mindful Moments
+//
+//  Created by Aanya Mahapatra on 15/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Mindful_MomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
