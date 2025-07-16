1ebd7efd10acef32950e8239110370c642b187b0
Catriona Eve Rondario <catrionaeve11@gmail.com>
2025-07-16 10:18:04 -0400

    i will crash out if this does not save

--- Mindful Moments/ContentView.swift
+++ Mindful Moments/ContentView.swift
@@ -9,35 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var name = ""
+    @State private var password = ""
     @State private var textTitle = "Welcome back."
     var body: some View {
         VStack {
             withAnimation {
                 Text(textTitle)
-
+                .font(.largeTitle)
+                .foregroundColor(.green)
+                .bold()
             }
             .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             TextField("Type your name here...", text: $name)
-                .font(.title)
-                .multilineTextAlignment(.center)
+                .padding()
                 .border(Color.gray, width: 4)
-            Button("Submit Name") {
-                withAnimation{
-                    textTitle = "Welcome to Mindful Moments, \(name)!"
-                        .font(.title)
-                        .foregroundColor(.green)
+            TextField("Type your password here...", text: $password)
+                .padding()
+                .border(Color.gray, width: 4)
+         
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Label Content@*/Text("Navigate")/*@END_MENU_TOKEN@*/
                 }
-                print(name)
+            }
+
+
+
             }
             .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             .font(.title2)
             .buttonStyle(.borderedProminent)
             .tint(.green)
         }
-        .padding()
+        //.padding()
         
     }
-}
+
         
         #Preview {
             ContentView()
