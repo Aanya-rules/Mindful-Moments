e4a97fbf6cdae4b5cde52e0be0d86dba5d28887f
Catriona Eve Rondario <catrionaeve11@gmail.com>
2025-07-16 11:43:20 -0400

    i got silly with the colors

--- Mindful Moments/ContentView.swift
+++ Mindful Moments/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var name = ""
     @State private var password = ""
-    @State private var textTitle = "Welcome back."
+    @State private var textTitle = "Welcome"
     var body: some View {
         NavigationStack {
         ZStack {
@@ -18,10 +18,16 @@ struct ContentView: View {
                 .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fill).edgesIgnoringSafeArea(.all)
             VStack {
+                Text("Mindful Moments")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color(hue: 0.504, saturation: 0.82, brightness: 0.604))
+                    
+                    
                 withAnimation {
                     Text(textTitle)
                         .font(.largeTitle)
-                        .foregroundColor(.green)
+                        .foregroundColor(Color(hue: 0.324, saturation: 0.531, brightness: 0.558))
                         .bold()
                 }
                 .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
@@ -44,7 +50,7 @@ struct ContentView: View {
                     
                 }
 
-                .border(Color.gray, width: 4)
+                .border(Color(hue: 0.282, saturation: 0.822, brightness: 0.486), width: 4)
                 .cornerRadius(5)
                 
                 
