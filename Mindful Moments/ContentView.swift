a50a18bb9c0df75154659694db031ae2407b8a09
Catriona Eve Rondario <catrionaeve11@gmail.com>
2025-07-15 22:27:50 -0400

    Tried to stylize and failed.

--- Mindful Moments/ContentView.swift
+++ Mindful Moments/ContentView.swift
@@ -1,24 +1,45 @@
 //
 //  ContentView.swift
-//  Mindful Moments
+//  InteractiveUI
 //
-//  Created by Aanya Mahapatra on 15/07/2025.
+//  Created by Scholar on 7/9/25.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    @State private var textTitle = "Welcome back."
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            withAnimation {
+                Text(textTitle)
+
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            TextField("Type your name here...", text: $name)
+                .font(.title)
+                .multilineTextAlignment(.center)
+                .border(Color.gray, width: 4)
+            Button("Submit Name") {
+                withAnimation{
+                    textTitle = "Welcome to Mindful Moments, \(name)!"
+                        .font(.title)
+                        .foregroundColor(.green)
+                }
+                print(name)
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.green)
         }
         .padding()
+        
     }
 }
+        
+        #Preview {
+            ContentView()
+        }
 
-#Preview {
-    ContentView()
-}
