b2ebae7d28ed30fe4aa96d4b6501c7d4fd59fbaf
Catriona Eve Rondario <catrionaeve11@gmail.com>
2025-07-16 21:50:30 -0400

    Fixed background and made text s n a z z y

--- Mindful Moments/ClosingPage.swift
+++ Mindful Moments/ClosingPage.swift
@@ -9,40 +9,47 @@ import SwiftUI
 
 struct ClosingPage: View {
     var body: some View {
-        VStack {
-            Text("Be kind to yourself!")
-                .font(.largeTitle)
-                .foregroundColor(Color(hue: 0.33, saturation: 0.669, brightness: 0.598))
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-                .bold()
-            
-            Text("Remember to:")
-                .font(.title2)
-                .fontWeight(.black)
-                .foregroundColor(Color(hue: 0.471, saturation: 0.721, brightness: 0.503))
-                .padding()
-            
-            Text("1. Practice deep breathing.")
-                .fontWeight(.heavy)
-                .multilineTextAlignment(.center)
-                .padding()
-            Text("It helps more than you think.")
-                .padding()
-            Text("2. Speak positive affirmations loud.")
-                .fontWeight(.heavy)
-                .padding()
-                .multilineTextAlignment(.center)
-            Text("Sometimes, it's better to hear them out loud.")
-                .padding()
-                .multilineTextAlignment(.center)
-            
-            Text("3. Don't forget, somebody cares about you!")
-                .fontWeight(.heavy)
-                .multilineTextAlignment(.center)
-                .padding()
-            Text("So, let someone know you care about them too.")
-                .fontWeight(.regular)
-                .padding()
+        ZStack {
+            Image("background")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fill).edgesIgnoringSafeArea(.all)
+            VStack {
+                
+                Text("Be kind to yourself!")
+                    .font(.largeTitle)
+                    .foregroundColor(Color(hue: 0.33, saturation: 0.669, brightness: 0.598))
+                    .bold()
+                
+                Text("Remember to:")
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(Color(hue: 0.471, saturation: 0.721, brightness: 0.503))
+                
+                Text("1. Practice deep breathing.")
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .foregroundColor(Color(hue: 0.471, saturation: 0.721, brightness: 0.503))
+                Text("It helps more than you think.")
+                    .padding()
+                Text("2. Speak positive affirmations loud.")
+                    .fontWeight(.heavy)
+                    .padding()
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(Color(hue: 0.471, saturation: 0.721, brightness: 0.503))
+                Text("Sometimes, it's better to hear them out loud.")
+                    .padding()
+                    .multilineTextAlignment(.center)
+                
+                Text("3. Don't forget, somebody cares about you!")
+                    .fontWeight(.heavy)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .foregroundColor(Color(hue: 0.471, saturation: 0.721, brightness: 0.503))
+                Text("So, let someone know you care about them too.")
+                    .fontWeight(.regular)
+                    .padding()
+            }
         }
     }
 }
