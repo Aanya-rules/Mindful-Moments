ee94fd9b26ae63beccc9f76ae56515df98d917c5
Emily Huang <emihuang1018@gmail.com>
2025-07-17 21:50:22 -0400

    code for destinations

--- Mindful Moments/WeeklyandDestinations.swift
+++ Mindful Moments/WeeklyandDestinations.swift
@@ -8,11 +8,47 @@
 import SwiftUI
 
 struct WeeklyandDestinations: View {
+    @State private var messageString: String = "Central Park, NYC"
+    @State private var imageName: String = "image0"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(0)
+                .shadow(radius: 40)
+                .padding()
+            
+            Spacer()
+            
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.green)
+                .frame(height: 150)
+                .frame(maxWidth: .infinity)
+                .padding()
+            
+            Spacer()
+            
+            Button("Show Destinations!") {
+                if imageName == "image0" {
+                    imageName = "image1"
+                    messageString = "Brooklyn Botanic Garden, NYC"
+                } else {
+                    imageName = "image0"
+                    messageString = "Central Park, NYC"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .padding(.bottom)
+        }
     }
 }
 
 #Preview {
-    WeeklyandDestinations()
+    WeeklyandDestinations ()
 }
