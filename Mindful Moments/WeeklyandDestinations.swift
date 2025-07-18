c6c592de24226ff182ac9492d1aafa7e65571c7a
Catriona Eve Rondario <catrionaeve11@gmail.com>
2025-07-18 11:20:33 -0400

    Fixed Login screen, added Destinations code. Ready to rock and roll!!!

--- Mindful Moments/WeeklyandDestinations.swift
+++ Mindful Moments/WeeklyandDestinations.swift
@@ -1,18 +1,53 @@
-//
-//  WeeklyandDestinations.swift
-//  Mindful Moments
-//
-//  Created by Aanya Mahapatra on 15/07/2025.
-//
-
 import SwiftUI
 
 struct WeeklyandDestinations: View {
+    @State private var messageString: String = "Central Park, NYC"
+    @State private var imageName: String = "image0"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Image("background 3")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fill)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image(imageName)
+                    .resizable()
+                    .scaledToFit()
+                    .cornerRadius(0)
+                    .shadow(radius: 40)
+                    .padding()
+                
+                Spacer()
+                
+                Text(messageString)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.green)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                
+                Spacer()
+                
+                Button("Show Destinations!") {
+                    if imageName == "image0" {
+                        imageName = "image1"
+                        messageString = "Brooklyn Botanic Garden, NYC"
+                    } else {
+                        imageName = "image0"
+                        messageString = "Central Park, NYC"
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+                .padding(.bottom)
+            }
+        }
     }
 }
-
 #Preview {
-    WeeklyandDestinations()
+    WeeklyandDestinations ()
 }
